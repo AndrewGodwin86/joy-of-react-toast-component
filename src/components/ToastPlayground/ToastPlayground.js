a1347c5f77c9fdf05bb926f71c06612074ac72a2
Andrew Godwin <andrew.godwin86@gmail.com>
2023-02-07 20:35:34 -0800

    Toast renders with icon and variant styles

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -16,7 +16,7 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
-      <Toast content={message} />
+      <Toast content={message} variant={variant} />
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
